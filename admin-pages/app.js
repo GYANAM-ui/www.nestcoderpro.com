07b8677a28a15dc2f2bb320d5a7abfceeaef890c
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-06-22 23:51:07 +0530

    Add files via upload

---
+++ admin-pages/app.js
@@ -0,0 +1,30 @@
+username = localStorage.getItem("username");
+useremail = localStorage.getItem("useremail");
+phone_number = localStorage.getItem("number");
+
+newPageTitle = username + "'s " + ' Nest Coder Pro';
+document.querySelector('title').textContent = newPageTitle;
+
+function gotovscode() {
+    window.open("https://code.visualstudio.com/download");
+}
+
+function pythonWebsite() {
+    window.open("https://www.python.org/");
+}
+
+function pyCharm() {
+    window.open("https://www.jetbrains.com/pycharm/download/");
+}
+
+function googleDev() {
+    window.open("https://www.google.com/intl/en_in/chrome/dev/");
+}
+
+function androidAPP() {
+    window.open("https://");
+}
+
+function chat() {
+    window.open("./group-chat/chat.html");
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function start() {
+    username = document.getElementById("name").value;
+    useremail = document.getElementById("email").value;
+    phone_number = document.getElementById("number").value;
+
+    if (username, useremail == "") {
+        document.getElementById("error_span").innerHTML = "<i class='fa fa-warning'>&nbsp;</i>" + "Please give correct details";
+    } else {
+        localStorage.setItem("username", username);
+        localStorage.setItem("useremail", useremail);
+        localStorage.setItem("number", phone_number);
+
+        window.location = "admin-pages/admin.html";
+    }
+}
