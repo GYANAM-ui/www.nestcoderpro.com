ec38b7227bfa8a7034ff04cd66d8ac9b0d4721d2
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-07-01 21:17:27 +0530

    Add files via upload

--- admin-pages/livestreams/live.js
+++ admin-pages/livestreams/live.js
@@ -1,10 +1,10 @@
-// function chatEnabled() {
-//     newWindow = window.open("https://deadsimplechat.com/sQog9mLCK", null, "height=400,width=350,status=yes,toolbar=no,menubar=no,location=no");
-// }
-
 function chatEnabled() {
-    window.alert("Chat is only available in Livestream");
+    newWindow = window.open("https://deadsimplechat.com/sQog9mLCK", null, "height=400,width=350,status=yes,toolbar=no,menubar=no,location=no");
 }
+
+// function chatEnabled() {
+//     window.alert("Chat is only available in Livestream");
+// }
 
 function leaveLiveStream() {
     window.location = "../admin.html";
